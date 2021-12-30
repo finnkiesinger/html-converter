2473018db6c0832b9c3211d88dfda3f717c63ac1
finnkiesinger <finnkiesinger@gmail.com>
2021-12-30 13:19:38 +0100

    publish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [url, setUrl] = useState();
+  const [name, setName] = useState();
+
+  const onChange = (e) => {
+    if (e.target.files.length <= 0) return;
+    const name = e.target.files[0].name.split('.')[0];
+    const reader = new FileReader();
+    reader.readAsText(e.target.files[0]);
+    reader.onload = async (e) => {
+      setName(name+".txt");
+      var file = new File([e.target.result], name+".txt", {
+        type: "text/plain",
+      });
+      setUrl(URL.createObjectURL(file));
+    };
+  };
+
+  return <div className="center">
+    <div style={{width: "100%"}}>
+      <label htmlFor="upload">Datei hochladen</label>
+      <input type="file" name="upload" id="upload" accept=".html" onChange={onChange} />
     </div>
-  );
+    {!!url && <a href={url} className="button" download={name}>
+      Konvertieren
+    </a>}
+  </div>;
 }
 
 export default App;
